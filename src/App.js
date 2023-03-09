892b4074eff80cf7d57e307a377ed3b4c67afbc2
Tana Luciya joji <tana.joji@gmail.com>
2023-03-09 17:51:09 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {NavLink} from 'react-router-dom'
+const App = () => { // 3 app has 2 parts -> navigation and routes -- dont use anchor tags in nav with react router dom since it refreshes the page
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>The Clothing Company</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </nav>
     </div>
   );
 }
